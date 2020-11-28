db8b690bf5ca7888af97b0f7f82cfeb6af410847
ali-ramadhan <ali.hh.ramadhan@gmail.com>
2020-11-28 18:48:45 -0500

    Test both even and odd

--- 1d_dct_test.jl
+++ 1d_dct_test.jl
@@ -6,24 +6,25 @@ using TransformPlayground
 
 Random.seed!(0)
 
-N = 5
 FT = Float64
 ε = eps(FT)
 
-A_cpu = randn(Float64, N)
-A_gpu = CuArray(A_cpu)
+for N in (4, 5)
+    A_cpu = randn(Float64, N)
+    A_gpu = CuArray(A_cpu)
 
-B_cpu = FFTW.r2r(A_cpu, FFTW.REDFT10)
-B_gpu = TransformPlayground.dct_makhoul_1d(A_gpu)
+    B_cpu = FFTW.r2r(A_cpu, FFTW.REDFT10)
+    B_gpu = TransformPlayground.dct_makhoul_1d(A_gpu)
 
-@test isapprox.(B_cpu, Vector(B_gpu), atol=2ε) |> all
+    @test isapprox.(B_cpu, Vector(B_gpu), atol=2ε) |> all
 
-C_cpu = FFTW.r2r(B_cpu, FFTW.REDFT01)
-C_cpu = C_cpu ./ 2N
+    C_cpu = FFTW.r2r(B_cpu, FFTW.REDFT01)
+    C_cpu = C_cpu ./ 2N
 
-C_gpu = TransformPlayground.idct_makhoul_1d(B_gpu)
+    C_gpu = TransformPlayground.idct_makhoul_1d(B_gpu)
 
-@test isapprox.(C_cpu, Vector(C_gpu), atol=ε) |> any
+    @test isapprox.(C_cpu, Vector(C_gpu), atol=ε) |> any
 
-@test isapprox.(A_cpu, C_cpu, atol=ε) |> all
-@test isapprox.(Vector(A_gpu), Vector(C_gpu), atol=ε) |> all
+    @test isapprox.(A_cpu, C_cpu, atol=ε) |> all
+    @test isapprox.(Vector(A_gpu), Vector(C_gpu), atol=ε) |> all
+end
